40554f1f6f91162a486f90e3a09fee74c9a7c536
Adarsh Kumbar <adarshkumbarbgm@gmail.com>
2024-01-03 17:35:00 +0530

    Done with Skills Section

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Hero from '@/components/main/Hero'
+import Skill from '@/components/main/Skill'
 import Image from 'next/image'
 
 export default function Home() {
@@ -6,6 +7,7 @@ export default function Home() {
     <main className='h-full w-full'>
       <div className="flex flex-col gap-20">
         <Hero/>
+        <Skill/>
       </div>
     </main>
   )
