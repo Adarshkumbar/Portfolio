1ab876400ebf1917d7ed085163ded76299080ef3
Adarsh Kumbar <adarshkumbarbgm@gmail.com>
2024-01-18 22:39:36 +0530

    footer

--- components/main/Footer.tsx
+++ components/main/Footer.tsx
@@ -10,7 +10,7 @@ import { SiGmail } from "react-icons/si";
 
 const Footer = () => {
   return (
-    <div className='w-full h-full bg-transparent text-gray-200 shadow-lg p-[15px]'>
+    <div className='w-full h-full bg-transparent text-gray-200 shadow-lg p-[15px] cursor-pointer'>
         <div className='w-full flex flex-col items-center justify-center m-auto'>
             <div className='w-full h-full flex flex-row items-center justify-around flex-wrap'>
                 <div className='min-w-[200px] h-auto flex flex-col items-center justify-start cursor-pointer'>
@@ -42,12 +42,12 @@ const Footer = () => {
                         <span className='text-[15px] ml-[6px]'> LinkedIn </span>
                     </p>
                     </Link>
-
-                    <a href={"adarshkumbarbgm@gmail.com"} target='_blank'>
-                    <p className='flex flex-row items-center my-[15px] cursor-pointer'>
+                    
+                    <a href="mailto:adarshkumbarbgm@gmail.com"  style={{ zIndex: 10 }}>
+                        <p className='flex flex-row items-center my-[15px] cursor-pointer' >
                         <SiGmail/>
                         <span className='text-[15px] ml-[6px]'> Gmail</span>
-                    </p>
+                        </p>
                     </a>
                 </div>
                 
@@ -56,7 +56,9 @@ const Footer = () => {
             <div className='mb-[20px] text-[15px] text-center'>
                 &copy; Adarsh Kumbar 2024 Inc.
             </div>
-
+            {/* <div className='mb-[20px] text-[15px] text-center'>
+                adarshkumbarbgm@gmail.com
+            </div> */}
         </div>
     </div>
   )
