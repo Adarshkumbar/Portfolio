645bd9730824f7b6e9137be48a0766cc6d5cf797
Adarsh Kumbar <adarshkumbarbgm@gmail.com>
2024-01-18 20:07:07 +0530

    font of nav changed

--- components/main/Navbar.tsx
+++ components/main/Navbar.tsx
@@ -56,7 +56,7 @@ const Navbar = () => {
             
 
             <div className='w-[500px] h-full flex flex-row items-center justify-between md:mr-20'>
-                <div className='flex item-center justify-between w-full h-auto border border-[#7042f861] bg-[#0300145e] mr-[15px] px-[20px] py-[10px] rounded-full text-gray-200'>
+                <div className='flex item-center justify-between w-full h-auto border border-[#7042f861] bg-[#0300145e] mr-[10px] text-sm md:text-lg px-[20px] py-[10px] rounded-full text-gray-200'>
                     <a href="#about" className='cursor-pointer'>About</a>
                     {/* <Link href="/" className='cursor-pointer'>About Me</Link> */}
 
