5b06e436b1cac874461edf4d16c524c5dce2f624
Adarsh Kumbar <adarshkumbarbgm@gmail.com>
2024-01-30 17:18:50 +0530

    updated footer

--- components/main/Footer.tsx
+++ components/main/Footer.tsx
@@ -9,46 +9,52 @@ import Link from 'next/link';
 import { SiGmail } from "react-icons/si";
 
 const Footer = () => {
+  const changeColor = (e : any ) =>{
+    e.target.style.color = 'yellow'
+}
+const changeColorE = (e : any ) =>{
+    e.target.style.color = '';
+}
   return (
     <section id="footer">
-    <div className="flex flex-row relative items-center justify-center min-h-screen w-full h-full py-20 border-style: solid;">
-      <div className="absolute w-auto h-auto top-0 z-[5] top-20">
+    <div className="flex flex-row relative items-center justify-center min-h-screen w-full h-full py-20">
+      <div className="absolute w-auto h-auto z-[5] top-20 ">
           <span className="text-transparent text-[30px] bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 ">
             CONTACT ME 
           </span>
       </div>
-      <div className="flex flex-col top-[230px] items-center justify-left translate-y-[-50px] absolute z-[20] w-auto h-auto ">
-        <div className="flex flex-col text-left group cursor-pointer w-auto h-auto ">
-               
+      <div className="flex flex-col top-[230px] items-center justify-left translate-y-[-50px] absolute z-[20] w-auto h-auto">
+        <div className="flex flex-col items-center text-left group cursor-pointer w-full h-auto " >
+        
             <Link href={"https://github.com/Adarshkumbar"} target='_blank'>
-                    <p className='flex flex-row items-center my-[5px] cursor-pointer z-[30] text-gray-200'>
+                    <p className='flex flex-row items-center text-[20px] my-[25px] cursor-pointer text-gray-200 '>
                             <RxGithubLogo/>
-                             <span className='text-[15px] ml-[6px] ' > Github </span>
+                             <span className=' ml-[6px] ' onMouseEnter={changeColor} onMouseLeave={changeColorE}> Github </span>
                      </p>
             </Link>
 
 
            <Link href={"https://discord.gg/Z2kNx7EcJP"} target="_blank" >
-                         <p className='flex flex-row items-center my-[25px] cursor-pointer text-gray-200'>
-                         <RxDiscordLogo/>
-                         <span className='text-[15px] ml-[6px]' > Discord </span>
+                         <p className='flex flex-row text-[20px] items-center my-[25px] cursor-pointer text-gray-200'>
+                         <RxDiscordLogo />
+                         <span className='ml-[6px]' onMouseEnter={changeColor} onMouseLeave={changeColorE}> Discord </span>
                          </p>
                         
             </Link>
 
            <Link href={'https://www.linkedin.com/in/adarshkumbar/'} target='_blank'>
-                     <p className='flex flex-row items-center my-[25px] cursor-pointer text-gray-200'>
+                     <p className='flex flex-row items-center my-[25px] text-[20px] cursor-pointer text-gray-200'>
                          <RxLinkedinLogo/>
-                         <span className='text-[15px] ml-[6px] ' > LinkedIn </span>
+                         <span className=' ml-[6px] ' onMouseEnter={changeColor} onMouseLeave={changeColorE}> LinkedIn </span>
                      </p>
             </Link>
 
-            <a href="mailto:adarshkumbarbgm@gmail.com"  style={{ zIndex: 10 }}>
-                         <p className='flex flex-row items-center my-[25px] cursor-pointer text-gray-200' >
+            <Link href="mailto:adarshkumbarbgm@gmail.com"  style={{ zIndex: 10 }}>
+                         <p className='flex flex-row items-center text-[20px] my-[25px] cursor-pointer text-gray-200' >
                          <SiGmail/>
-                         <span className='text-[15px] ml-[6px] text-gray-200' > Gmail</span>
+                         <span className='ml-[6px] text-gray-200' onMouseEnter={changeColor} onMouseLeave={changeColorE}> Gmail</span>
                          </p>
-            </a>
+            </Link>
 
             <br></br>
             
